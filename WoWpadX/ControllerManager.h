4320f71fb684da417a3bce4644e943d3785d0a4c
Dexton Armstrong <dextarm@gmail.com>
2026-07-28 14:05:45 -0600

    Refactor controller lifecycle for Xbox full-screen startup

--- WoWpadX/ControllerManager.h
+++ WoWpadX/ControllerManager.h
@@ -2,14 +2,21 @@
 
 #pragma once
 
+#include <atomic>
 #include <mutex>
-#include <SDL3/SDL_gamepad.h>
+#include <thread>
 #include <vector>
+
 #include <QObject>
+#include <SDL3/SDL_gamepad.h>
+
 #include "Gamepad/gamepad.h"
+
+class QTimer;
 
 class ControllerManager : public QObject {
     Q_OBJECT
+
 public:
     std::atomic<float> m_touchDeltaX{ 0 };
     std::atomic<float> m_touchDeltaY{ 0 };
@@ -18,26 +25,6 @@ public:
 
     static ControllerManager* instance();
 
-
-private: 
-    static ControllerManager* m_instance;
-
-    std::mutex controllerMutex;
-    std::vector<SDL_Gamepad*> controllers;
-    SDL_Gamepad* activeController = nullptr;
-    SDL_JoystickID activeInstanceId = -1;
-
-    std::atomic<bool> threadRunning = false;
-    std::thread watcherThread;
-
-    int lastBatteryWarn = 100;
-    bool showedBatteryLow = false;
-    bool showedBatteryCritical = false;
-
-    void HandleEvents();
-    void ControllerWatcher();
-
-public:
     int GetBatteryLevel(SDL_Gamepad* pad);
     QString GetButtonIcon(GamepadBinding button);
     QString GetButtonName(GamepadBinding button);
@@ -46,11 +33,33 @@ public:
     void startWatcher();
     void stopWatcher();
     SDL_Gamepad* getActiveController();
-    const std::vector<SDL_Gamepad*>& getControllers();
+    std::vector<SDL_Gamepad*> getControllers();
     bool Rumble(uint16_t low_freq, uint16_t high_freq, uint32_t duration_ms);
-
 
 signals:
     void activeControllerChanged();
     void controllersChanged();
+
+private:
+    static ControllerManager* m_instance;
+
+    std::mutex controllerMutex;
+    std::vector<SDL_Gamepad*> controllers;
+    std::vector<SDL_Gamepad*> retiredControllers;
+    SDL_Gamepad* activeController = nullptr;
+    SDL_JoystickID activeInstanceId = -1;
+
+    std::atomic<bool> threadRunning = false;
+    std::thread watcherThread;
+    QTimer* eventTimer = nullptr;
+    bool sdlInitialized = false;
+
+    int lastBatteryWarn = 100;
+    bool showedBatteryLow = false;
+    bool showedBatteryCritical = false;
+
+    void PumpEvents();
+    void ReconcileControllers();
+    void ControllerWatcher();
+    void CheckBatteryWarnings();
 };
